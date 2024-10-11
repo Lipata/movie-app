9e9755c76bd849c40d1cce3aac430108e897b58b
Nikolay Alipiev <nalipiev@infragistics.com>
2024-10-11 16:13:12 +0300

    Movie App initial version

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IGX_CARD_DIRECTIVES, IGX_DATE_PICKER_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_LIST_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_TABS_DIRECTIVES, IgxAvatarComponent, IgxButtonDirective, IgxIconButtonDirective, IgxIconComponent, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective } from '@infragistics/igniteui-angular';
+import { Subject, takeUntil } from 'rxjs';
+import { TheatresNearYouType } from '../models/movie-app-data/theatres-near-you-type';
+import { ShowtimesType } from '../models/movie-app-data/showtimes-type';
+import { TheatresType } from '../models/movie-app-data/theatres-type';
+import { NowPlayingType } from '../models/movie-app-data/now-playing-type';
+import { MovieListType } from '../models/movie-app-data/movie-list-type';
+import { MovieAppDataService } from '../services/movie-app-data.service';
+
+@Component({
+  selector: 'app-movies',
+  standalone: true,
+  imports: [IGX_INPUT_GROUP_DIRECTIVES, IGX_DATE_PICKER_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_TABS_DIRECTIVES, IGX_CARD_DIRECTIVES, IGX_LIST_DIRECTIVES, IgxToggleActionDirective, IgxOverlayOutletDirective, IgxIconButtonDirective, IgxButtonDirective, IgxRippleDirective, IgxToggleDirective, IgxIconComponent, IgxAvatarComponent, FormsModule],
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.scss']
+})
+export class MoviesComponent implements OnInit, OnDestroy {
+  private destroy$: Subject<void> = new Subject<void>();
+  public movieAppDataNowPlaying: NowPlayingType[] = [];
+  public value: string = '1';
+  public movieAppDataMovieList: MovieListType[] = [];
+  public movieAppDataTheatres: TheatresType[] = [];
+  public movieAppDataShowtimes: ShowtimesType[] = [];
+  public movieAppDataTheatresNearYou: TheatresNearYouType[] = [];
+
+  constructor(
+    private movieAppDataService: MovieAppDataService,
+  ) {}
+
+  ngOnInit() {
+    this.movieAppDataService.getNowPlayingList().pipe(takeUntil(this.destroy$)).subscribe(
+      data => this.movieAppDataNowPlaying = data
+    );
+    this.movieAppDataService.getMovieListList().pipe(takeUntil(this.destroy$)).subscribe(
+      data => this.movieAppDataMovieList = data
+    );
+    this.movieAppDataService.getTheatresList().pipe(takeUntil(this.destroy$)).subscribe(
+      data => this.movieAppDataTheatres = data
+    );
+    this.movieAppDataService.getShowtimesList().pipe(takeUntil(this.destroy$)).subscribe(
+      data => this.movieAppDataShowtimes = data
+    );
+    this.movieAppDataService.getTheatresNearYouList().pipe(takeUntil(this.destroy$)).subscribe(
+      data => this.movieAppDataTheatresNearYou = data
+    );
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
